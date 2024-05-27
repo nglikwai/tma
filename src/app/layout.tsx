b8e0f047a361887092ae3800c63f4fd3a42d73ae
LIK WAI <nglikwai@ymail.com>
2024-05-27 01:16:36 +0100

    update: ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,12 @@ interface Props {
     readonly children: ReactNode;
 }
 
+export const metadata = {
+    title: "Treasury Markets Association",
+    description:
+        "The Treasury Markets Association (TMA) was incorporated through the institutionalisation of the Treasury Markets Forum of Hong Kong and the merger with ACI-The Financial Markets Association of Hong Kong in 2005. Principal functions of the TMA include promoting co-operation and synergy among market practitioners with a view to enhancing professionalism and the overall competitiveness of the treasury markets in Hong Kong, thereby maintaining the role as an international financial centre."
+};
+
 export default function RootLayout({ children }: Props) {
     return (
         <html lang="en">
